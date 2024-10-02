8cb4eb6cd5daaa01639b6ec43e29723632891b5a
armr14 <a.morillo@uniandes.edu.co>
2024-10-02 14:48:56 -0500

    13 full

--- Tarea álgebra lineal/5.7.13.py
+++ Tarea álgebra lineal/5.7.13.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def GetJacobian(f, x, h=1e-3):
+def GetJacobian2(f, x, h):
     m = len(f)
     n = x.shape[0]
     J = np.zeros((m, n))
@@ -17,20 +17,37 @@ def GetJacobian(f, x, h=1e-3):
       
     return J
 
-def evalf(f,x):
-    n = len(f)
-    vector = np.array([])
-    
-    for i in range(n):
-        vector = np.append(vector,f[i](*x))
-        
-    return vector
+def GetJacobian4(f, x, h):
+    m = len(f)
+    n = x.shape[0]
+    J = np.zeros((m, n))
+  
+    for i in range(m):
+        for j in range(n):
+            rf2 = x.copy()
+            rf = x.copy()
+            rb = x.copy()
+            rb2 = x.copy()
+      
+            rf2[j] += 2 * h
+            rf[j] += h
+            rb[j] -= h
+            rb2[j] -= 2 * h
+            
+            f_rf2 = f[i](*rf2)
+            f_rf = f[i](*rf)
+            f_rb = f[i](*rb)
+            f_rb2 = f[i](*rb2)
+            
+            J[i, j] = (-f_rf2 + 8 * f_rf - 8 * f_rb + f_rb2) / (12 * h)
+      
+    return J
 
 Sistema=(lambda x1,x2,x3: 6*x1 - 2*np.cos(x2*x3) - 1,\
             lambda x1,x2,x3: 9*x2 + np.sqrt(x1**2 + np.sin(x3)+1.06) + 0.9,\
             lambda x1,x2,x3: 60*x3 + 3*np.exp(-x1*x2) + 10*np.pi - 3)
 x0 = np.array([0.5,0.5,0.5])
 
-print("Jacobiano de segundo orden h = 0.01: ", GetJacobian(Sistema,x0))
-print("Jacobiano de cuarto orden h = 0.01", GetJacobian(Sistema,x0))
-print("Jacobiano de segundo orden h = 0.0001: ", GetJacobian(Sistema,x0))
+print("Jacobiano de segundo orden h = 0.01:\n", GetJacobian2(Sistema,x0,0.01))
+print("\nJacobiano de cuarto orden h = 0.01:\n", GetJacobian4(Sistema,x0,0.01))
+print("\nJacobiano de segundo orden igualado con h = 0.00001:\n", GetJacobian2(Sistema,x0,0.00001))
