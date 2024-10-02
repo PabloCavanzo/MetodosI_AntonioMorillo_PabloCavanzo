97f6d1875d812987de7f2b9d9e383764b709854d
armr14 <a.morillo@uniandes.edu.co>
2024-10-02 13:06:17 -0500

    prints

--- Tarea álgebra lineal/5.7.4.py
+++ Tarea álgebra lineal/5.7.4.py
@@ -3,16 +3,24 @@ import numpy as np
 def mult_matrices(M1,M2):
     m = M1.shape[0]
     p = M2.shape[1]
-    n = M1.shape[0]
+    n = M1.shape[1]
+    
     M = np.zeros((m,p))
     M = M.astype(float)
     
     for i in range(m):
         for j in range(p):
             for k in range(n):
-                M[i][j] += M1[i][k] * M2[k][j] 
+                M[i][j] += M1[i][k] * M2[k][j]
             
     return M
+
+def generar_matrices(m, n, p, min_val=-10, max_val=10):
+    A = np.random.randint(min_val, max_val+1, size=(m, n))
+    B = np.random.randint(min_val, max_val+1, size=(n, p))
+    return A, B
+A2,B2 = generar_matrices(5,4,7)
+
 
 A = np.array([[1,0,0],
               [5,1,0],
@@ -23,4 +31,4 @@ B = np.array([[4,-2,1],
               [0,0,2]])
 
 C = mult_matrices(A,B)
-print("Matriz A:\n",A,"\n\nMatriz B:\n",B,"\n\nMatriz A*B:\n",C)
+print("Matriz A:\n",A,"\n\nMatriz B:\n",B,"\n\nMatriz A*B:\n",C,"\n\nResultado numpy:\n", A @ B)
--- Tarea álgebra lineal/5.7.8.py
+++ Tarea álgebra lineal/5.7.8.py
@@ -1,18 +1,18 @@
 import numpy as np
 
 def factor_LU(M):
-    M = M.astype(float)
+    U = M.astype(float)
     n = M.shape[0]
     L = np.eye(n)
     
     for j in range(0,n-1):
         for i in range(j+1,n):
-            k = M[i][j]/M[j][j]
-            M[i] = M[i] - k*M[j]
+            k = U[i][j]/U[j][j]
+            U[i] = U[i] - k*U[j]
             
             L[i][j] = k 
         
-    return M,L
+    return L,U
 
 A = np.array([[4.,-2.,1.],[20.,-7.,12.],[-8.,13., 21.]])
-print("Matriz A:\n",A,"\n\nMatriz L:\n",factor_LU(A)[1],"\n\nMatriz U:\n",factor_LU(A)[0])
+print("Matriz A:\n",A,"\n\nMatriz L:\n",factor_LU(A)[0],"\n\nMatriz U:\n",factor_LU(A)[1])
