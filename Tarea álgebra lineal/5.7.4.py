b25cf370d1017dbf6118d461ba3c3e70299f9fe9
armr14 <a.morillo@uniandes.edu.co>
2024-10-01 22:01:11 -0500

    5.7.4 full

---
+++ Tarea álgebra lineal/5.7.4.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+def mult_matrices(M1,M2):
+    m = M1.shape[0]
+    p = M2.shape[1]
+    n = M1.shape[0]
+    M = np.zeros((m,p))
+    
+    for i in range(m):
+        for j in range(p):
+            for k in range(n):
+                M[i][j] += M1[i][k] * M2[k][j] 
+            
+    return M
+
+A = np.array([[1,0,0],
+              [5,1,0],
+              [-2,3,1]])
+
+B = np.array([[4,-2,1],
+              [0,3,7],
+              [0,0,2]])
+
+C = mult_matrices(A,B)
+print(C)
