3e81ec33600b53ca0bf8020f5e74ad0b5f090cf6
armr14 <a.morillo@uniandes.edu.co>
2024-10-08 21:29:10 -0500

    clase implementada

--- Tarea álgebra lineal/5.7.3.py
+++ Tarea álgebra lineal/5.7.3.py
@@ -1,48 +1,54 @@
 import numpy as np
 
-def Jacobi(A,b,x, itmax = 1000, tolerancia = 1e-16):
-    for it in range(itmax):
-        x_new = np.copy(x)
-              
-        for i in range(A.shape[0]):
-            suma = 0
-            for j in range(A.shape[1]):
-                if i != j:
-                    suma += A[i][j]*x[j]
+class SistemaLineal:
+    def __init__(self, A, b):
+        self.A = A
+        self.b = b
+
+    def Jacobi(self, x, itmax=1000, tolerancia=1e-16):
+        for it in range(itmax):
+            x_new = np.copy(x)     
+            for i in range(self.A.shape[0]):
+                suma = 0
+                for j in range(self.A.shape[1]):
+                    if i != j:
+                        suma += self.A[i][j] * x[j]
+                
+                x_new[i] = (self.b[i] - suma) / self.A[i][i]
+                        
+            if np.linalg.norm(np.dot(self.A, x_new) - self.b) < tolerancia:
+                break
+
+            x = np.copy(x_new)
             
-            x_new[i] = (b[i] - suma)/A[i][i]
-                    
-        if np.linalg.norm(np.dot(A,x_new)-b) < tolerancia:
-            break
+        return x, it
 
-        x = np.copy(x_new)
-        
-    return x,it
+    def GaussSeidel(self, x, itmax=1000, tolerancia=1e-16):
+        for it in range(itmax):
+            x_new = np.copy(x)
+            for i in range(self.A.shape[0]):
+                suma = 0
+                for j in range(self.A.shape[1]):
+                    if i != j:
+                        suma += self.A[i][j] * x_new[j]
+                
+                x_new[i] = (self.b[i] - suma) / self.A[i][i]
+                        
+            if np.linalg.norm(np.dot(self.A, x_new) - self.b) < tolerancia:
+                break
 
-def GaussSeidel(A,b,x, itmax = 100, tolerancia = 1e-16):
-    for it in range(itmax):
-        x_new = np.copy(x)
-              
-        for i in range(A.shape[0]):
-            suma = 0
-            for j in range(A.shape[1]):
-                if i != j:
-                    suma += A[i][j]*x_new[j]
+            x = np.copy(x_new)
             
-            x_new[i] = (b[i] - suma)/A[i][i]
-                    
-        if np.linalg.norm(np.dot(A,x_new)-b) < tolerancia:
-            break
+        return x, it
 
-        x = np.copy(x_new)
-        
-    return x,it
+A = np.array([[3, -1, -1],
+              [-1, 3, 1],
+              [2, 1, 4]], dtype=float)
+b = np.array([1., 3., 7.])
+x0 = np.array([0., 0., 0.])
 
-A = np.array([[3,-1,-1],[-1,3,1],[2,1,4]])
-b = np.array([1.,3.,7.])
-x0 = np.array([0.,0.,0.])
-
-xj,itj = Jacobi(A,b,x0)
-xg,itg = GaussSeidel(A,b,x0)
-
-print(xj,itj,xg,itg)
+sistema = SistemaLineal(A, b)
+xj, itj = sistema.Jacobi(x0)
+xg, itg = sistema.GaussSeidel(x0)
+print("Solución Jacobi:      " + str(xj) + " convirgió en " + str(itj) + " iteraciones.")
+print("Solución Gauss-Seidel: " + str(xg) + " convirgió en " + str(itg) + " iteraciones.")
